90d12ec846dca3546bbab9133dd2d817ddb74df3
syliesox <syliesox@gmail.com>
2019-06-25 16:23:13 -0400

    added spotify-this and movie-this and do-what-it-says command functionality

--- liri.js
+++ liri.js
@@ -1,12 +1,13 @@
 // Required NPM Packages
 require("dotenv").config();
-
-var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
 var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
 var Spotify = require("node-spotify-api");
+
+// Get Spotify keys that are stored in the keys.js file
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
 
 // User inputs
 // The command the user has chosen
@@ -14,17 +15,26 @@ var command = process.argv[2];
 // What the user wants to search with the command, grabs everything typed after the command
 var search = process.argv.slice(3).join(" ");
 
-if (command === "concert-this") {
-    concertInfo();
-} else if (command === "spotify-this-song") {
-    songInfo();
-} else if (command === "movie-this") {
-    movieInfo();
-} else if (command === "do-what-it-says") {
-    commandInfo();
-} else {
-    console.log("Command requested unknown to LIRI. Pick a proper LIRI command.")
-};
+runLIRI();
+
+// Setting up the command structure
+function runLIRI() {
+    if (command === "concert-this") {
+        concertInfo();
+    } else if (command === "spotify-this-song") {
+        songInfo();
+    } else if (command === "movie-this") {
+        movieInfo();
+    } else if (command === "do-what-it-says") {
+        doWhatItSays();
+    } else {
+        console.log("Command requested unknown to LIRI. Pick a proper LIRI command: ")
+        console.log("concert-this");
+        console.log("spotify-this-song");
+        console.log("movie-this");
+        console.log("do-what-it-says");
+    };
+}
 
 function concertInfo() {
     var queryURL = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
@@ -34,13 +44,12 @@ function concertInfo() {
 
         if (results.length !== 0) {
             console.log("===============================")
-            console.log("Upcoming events for " + search);
+            console.log("Upcoming events for '" + search + "'");
             for (var i = 0; i < results.length; i++) {
                 var concert = results[i];
                 console.log("Venue: " + concert.venue.name + ",");
                 console.log("Location: " + concert.venue.city + ", " + concert.venue.region);
                 console.log("Date: " + moment(concert.datetime).format("MM/DD/YYYY"));
-                console.log("Time: " + (moment(concert.datetime).hour()) );
                 if (moment(concert.datetime).hour() >= 12) {
                     var time = moment(concert.datetime).hour();
                     var newTime = time - 12;
@@ -58,26 +67,77 @@ function concertInfo() {
     })
 }
 
-// function songInfo() {
-//     var queryURL = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
+function songInfo() {
+    spotify.search({
+        type: "track",
+        query: search,
+        limit: 10
+    }).then(function(response) {
+        var results = response.tracks.items;
 
-//     axios.get(queryURL).then(function(response) {
-//         var results = response.data;
+        if (results.length !== 0) {
+            console.log("===============================");
+            console.log("RESULTS for '" + search + "'");
+            for (var i = 0; i < results.length; i++) {
+                console.log("===============================");
+                console.log("Result #" + i);
+                console.log("Artist: " + results[i].artists[0].name);
+                console.log("Song Name: " + results[i].name);
+                console.log("Album: ") + results[i].album.name;
+                console.log("Preview Song: " + results[i].preview_url);
+                console.log("===============================");
+            }
+        } else {
+            console.log("Ace of Base, The Sign");
+        };
+    })
+}
 
-//         if (results.length !== 0) {
-//             console.log("===============================")
-//             console.log("Upcoming events for " + search);
-//             for (var i = 0; i < results.length; i++) {
-//                 var concert = results[i];
-//                 console.log(concert.venue.name + ",");
-//                 console.log(concert.venue.city + ", " + concert.venue.region);
-//                 console.log(moment(concert.datetime).format("MM/DD/YYYY"));
-//                 console.log("===============================")
-//             };
-//         } else {
-//             console.log("No events found for " + search + ". Try again!")
-//         }
-//     })
-// }
+function movieInfo() {
+    var queryURL = "http://www.omdbapi.com/?t=" + search + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+
+    axios.get(queryURL).then(function(response) {
+        var results = response.data;
+
+        if (search === undefined) {
+            console.log("Mr. Nobody");
+        } else {
+            console.log("===============================");
+            console.log("Results for '" + search + "'");
+            console.log("===============================");
+            console.log("Title: " + results.Title);
+            console.log("Year: " + results.Year);
+            console.log("IMDB Rating: " + results.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + results.tomatoRating);
+            console.log("Country: " + results.Country);
+            console.log("Language(s): " + results.Language);
+            console.log("Plot: " + results.Plot);
+            console.log("Actors: " + results.Actors);
+            console.log("===============================");
+        }
+        
+    });
+}
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            console.log("Error: " + error);
+        }
+
+        var dataSplit = data.split(",");
+        console.log(dataSplit);
+        command = dataSplit[0];
+        search = dataSplit[1];
+
+        runLIRI();
+
+        // To test run in random.txt file:
+        // movie-this, "13 Going on 30"
+        // concert-this, "Maroon 5"
+        // spotify-this-song, "I Want it That Way"
+
+    })
+}
 
 
