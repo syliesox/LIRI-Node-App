e1cfded4785f7be34785e870c092e8a66b45de2a
syliesox <syliesox@gmail.com>
2019-06-25 12:55:48 -0400

    added Time to the concert-this command results display

--- liri.js
+++ liri.js
@@ -37,9 +37,19 @@ function concertInfo() {
             console.log("Upcoming events for " + search);
             for (var i = 0; i < results.length; i++) {
                 var concert = results[i];
-                console.log(concert.venue.name + ",");
-                console.log(concert.venue.city + ", " + concert.venue.region);
-                console.log(moment(concert.datetime).format("MM/DD/YYYY"));
+                console.log("Venue: " + concert.venue.name + ",");
+                console.log("Location: " + concert.venue.city + ", " + concert.venue.region);
+                console.log("Date: " + moment(concert.datetime).format("MM/DD/YYYY"));
+                console.log("Time: " + (moment(concert.datetime).hour()) );
+                if (moment(concert.datetime).hour() >= 12) {
+                    var time = moment(concert.datetime).hour();
+                    var newTime = time - 12;
+                    console.log("Time: " + newTime + " PM");
+                } else if (moment(concert.datetime).hour() = 12) {
+                    console.log("Time: " + (moment(concert.datetime).hour()) + " PM");
+                } else {
+                    console.log("Time: " + (moment(concert.datetime).hour()) + " AM");
+                }
                 console.log("===============================")
             };
         } else {
@@ -47,5 +57,27 @@ function concertInfo() {
         }
     })
 }
+
+// function songInfo() {
+//     var queryURL = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
+
+//     axios.get(queryURL).then(function(response) {
+//         var results = response.data;
+
+//         if (results.length !== 0) {
+//             console.log("===============================")
+//             console.log("Upcoming events for " + search);
+//             for (var i = 0; i < results.length; i++) {
+//                 var concert = results[i];
+//                 console.log(concert.venue.name + ",");
+//                 console.log(concert.venue.city + ", " + concert.venue.region);
+//                 console.log(moment(concert.datetime).format("MM/DD/YYYY"));
+//                 console.log("===============================")
+//             };
+//         } else {
+//             console.log("No events found for " + search + ". Try again!")
+//         }
+//     })
+// }
 
 
